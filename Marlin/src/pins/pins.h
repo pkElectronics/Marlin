87966140830f837c5c960f77623b83ec46ad4d16
pascal <pascal.krumme@pkelectronics.de>
2021-03-20 23:14:05 +0100

    Added compiler support for Sigmoid Boards

--- Marlin/src/pins/pins.h
+++ Marlin/src/pins/pins.h
@@ -625,6 +625,8 @@
   #include "stm32f4/pins_MKS_ROBIN2.h"          // STM32F4                                env:MKS_ROBIN2
 #elif MB(FYSETC_S6_V2_0)
   #include "stm32f4/pins_FYSETC_S6_V2_0.h"      // STM32F4                                env:FYSETC_S6
+#elif MB(IWB_PROGRESSR2D)
+  #include "stm32f4/pins_IWB_PROGRESSR2D.h"      // STM32F4                                env:IWB_PROGRESSR2D
 
 //
 // ARM Cortex M7
