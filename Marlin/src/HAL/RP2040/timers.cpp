eaef84681440584c458db81be9a3d1a064fc9d35
pkElectronics <pascalkrumme@gmail.com>
2021-08-29 12:50:41 +0200

    Added HAL Timer Framework
    Utilitzes the microsecond timer of the RP2040 and the corresponding alarm register to emulate standard mcu timers.

--- Marlin/src/HAL/RP2040/timers.cpp
+++ Marlin/src/HAL/RP2040/timers.cpp
@@ -25,12 +25,131 @@
 
 #include "../../inc/MarlinConfig.h"
 
+alarm_pool_t* HAL_timer_pool_0;
+alarm_pool_t* HAL_timer_pool_1;
+alarm_pool_t* HAL_timer_pool_2;
+alarm_pool_t* HAL_timer_pool_3;
+
+struct repeating_timer HAL_timer_0;
+struct repeating_timer HAL_timer_1;
+struct repeating_timer HAL_timer_2;
+struct repeating_timer HAL_timer_3;
+
+
+//alarm_callback_t HAL_timer_alarm_pool_0_callback(alarm_id_t id, void *user_data);
+//alarm_callback_t HAL_timer_alarm_pool_1_callback(alarm_id_t id, void *user_data);
+//alarm_callback_t HAL_timer_alarm_pool_2_callback(alarm_id_t id, void *user_data);
+//alarm_callback_t HAL_timer_alarm_pool_3_callback(alarm_id_t id, void *user_data);
+int64_t HAL_timer_alarm_pool_0_callback(long int, void*);
+int64_t HAL_timer_alarm_pool_1_callback(long int, void*);
+int64_t HAL_timer_alarm_pool_2_callback(long int, void*);
+int64_t HAL_timer_alarm_pool_3_callback(long int, void*);
+
+bool HAL_timer_repeating_0_callback(repeating_timer* timer);
+bool HAL_timer_repeating_1_callback(repeating_timer* timer);
+bool HAL_timer_repeating_2_callback(repeating_timer* timer);
+bool HAL_timer_repeating_3_callback(repeating_timer* timer);
+
 void HAL_timer_init() {
+
+  //reserve all the available alarm pools to use as "pseudo" hardware timers
+  //HAL_timer_pool_0 = alarm_pool_create(0,2);
+  HAL_timer_pool_1 = alarm_pool_create(1,6);
+  HAL_timer_pool_0 = HAL_timer_pool_1;
+  HAL_timer_pool_2 = alarm_pool_create(2,6);
+  HAL_timer_pool_3 = alarm_pool_create(3,6);
+
+  irq_set_priority(TIMER_IRQ_0,0xC0);
+  irq_set_priority(TIMER_IRQ_1,0x80);
+  irq_set_priority(TIMER_IRQ_2,0x40);
+  irq_set_priority(TIMER_IRQ_3,0x00);
+
+
+
+  //alarm_pool_init_default();
 }
 
 void HAL_timer_start(const uint8_t timer_num, const uint32_t frequency) {
-  switch (timer_num) {
-    default: break;
+  int64_t freq = (int64_t) frequency;
+  int64_t us = (1000000ull / freq)*-1ull;
+  bool returnvalue;
+  switch (timer_num){
+
+    case 0:
+      returnvalue = alarm_pool_add_repeating_timer_us(HAL_timer_pool_0,us,HAL_timer_repeating_0_callback, NULL, &HAL_timer_0);
+      break;
+
+    case 1:
+      returnvalue = alarm_pool_add_repeating_timer_us(HAL_timer_pool_1,us,HAL_timer_repeating_1_callback, NULL, &HAL_timer_1);
+      break;
+
+    case 2:
+      returnvalue = alarm_pool_add_repeating_timer_us(HAL_timer_pool_2,us,HAL_timer_repeating_2_callback, NULL, &HAL_timer_2);
+      break;
+
+    case 3:
+      returnvalue = alarm_pool_add_repeating_timer_us(HAL_timer_pool_3,us,HAL_timer_repeating_3_callback, NULL, &HAL_timer_3);
+      break;
   }
 }
+
+void  HAL_timer_stop(const uint8_t timer_num) {
+   switch (timer_num){
+    case 0:
+      cancel_repeating_timer(&HAL_timer_0);
+      break;
+
+    case 1:
+      cancel_repeating_timer(&HAL_timer_1);
+      break;
+
+    case 2:
+      cancel_repeating_timer(&HAL_timer_2);
+      break;
+
+    case 3:
+      cancel_repeating_timer(&HAL_timer_3);
+      break;
+  }
+}
+
+
+int64_t HAL_timer_alarm_pool_0_callback(long int, void*){
+  HAL_timer_0_callback();
+  return 0;
+}
+int64_t HAL_timer_alarm_pool_1_callback(long int, void*){
+  HAL_timer_1_callback();
+  return 0;
+}
+int64_t HAL_timer_alarm_pool_2_callback(long int, void*){
+  HAL_timer_2_callback();
+  return 0;
+}
+int64_t HAL_timer_alarm_pool_3_callback(long int, void*){
+  HAL_timer_3_callback();
+  return 0;
+}
+
+bool HAL_timer_repeating_0_callback(repeating_timer* timer){
+  HAL_timer_0_callback(); 
+  return true;
+}
+bool HAL_timer_repeating_1_callback(repeating_timer* timer){
+  HAL_timer_1_callback();
+  return true;
+}
+bool HAL_timer_repeating_2_callback(repeating_timer* timer){
+  HAL_timer_2_callback();
+  return true;
+}
+bool HAL_timer_repeating_3_callback(repeating_timer* timer){
+  HAL_timer_3_callback();
+  return true;
+}
+
+void __attribute__((weak)) HAL_timer_0_callback(){}
+void __attribute__((weak)) HAL_timer_1_callback(){}
+void __attribute__((weak)) HAL_timer_2_callback(){}
+void __attribute__((weak)) HAL_timer_3_callback(){}
 #endif // __PLAT_RP2040__
--- Marlin/src/HAL/RP2040/timers.h
+++ Marlin/src/HAL/RP2040/timers.h
@@ -6,16 +6,17 @@
 // Defines
 // ------------------------
 
-#define _HAL_TIMER(T) _CAT(LPC_TIM, T)
-#define _HAL_TIMER_IRQ(T) TIMER##T##_IRQn
-#define __HAL_TIMER_ISR(T) extern "C" void TIMER##T##_IRQHandler()
+//#define _HAL_TIMER(T) _CAT(LPC_TIM, T)
+//#define _HAL_TIMER_IRQ(T) TIMER##T##_IRQn
+//#define __HAL_TIMER_ISR(T) extern "C" alarm_callback_t HAL_timer_alarm_pool_##T##_callback()
+#define __HAL_TIMER_ISR(T) extern void HAL_timer_##T##_callback()
 #define _HAL_TIMER_ISR(T)  __HAL_TIMER_ISR(T)
 
-typedef uint32_t hal_timer_t;
+
+typedef uint64_t hal_timer_t;
 #define HAL_TIMER_TYPE_MAX 0xFFFFFFFF
 
-#define HAL_TIMER_RATE         ((F_CPU) / 4)  // frequency of timers peripherals
-
+#define HAL_TIMER_RATE         (1000000)  // fixed value as we use a microsecond timesource
 #ifndef STEP_TIMER_NUM
   #define STEP_TIMER_NUM        0  // Timer Index for Stepper
 #endif
@@ -33,8 +34,8 @@ typedef uint32_t hal_timer_t;
 #define TEMP_TIMER_FREQUENCY   1000 // temperature interrupt frequency
 
 #define STEPPER_TIMER_RATE     HAL_TIMER_RATE   // frequency of stepper timer (HAL_TIMER_RATE / STEPPER_TIMER_PRESCALE)
-#define STEPPER_TIMER_TICKS_PER_US ((STEPPER_TIMER_RATE) / 1000000) // stepper timer ticks per µs
-#define STEPPER_TIMER_PRESCALE (CYCLES_PER_MICROSECOND / STEPPER_TIMER_TICKS_PER_US)
+#define STEPPER_TIMER_TICKS_PER_US (1) // fixed value as we use a microsecond timesource
+#define STEPPER_TIMER_PRESCALE (1)
 
 #define PULSE_TIMER_RATE       STEPPER_TIMER_RATE   // frequency of pulse timer
 #define PULSE_TIMER_PRESCALE   STEPPER_TIMER_PRESCALE
@@ -55,16 +56,51 @@ typedef uint32_t hal_timer_t;
 #endif
 
 // Timer references by index
-#define STEP_TIMER_PTR _HAL_TIMER(STEP_TIMER_NUM)
-#define TEMP_TIMER_PTR _HAL_TIMER(TEMP_TIMER_NUM)
+//#define STEP_TIMER_PTR _HAL_TIMER(STEP_TIMER_NUM)
+//#define TEMP_TIMER_PTR _HAL_TIMER(TEMP_TIMER_NUM)
+
+extern alarm_pool_t* HAL_timer_pool_0;
+extern alarm_pool_t* HAL_timer_pool_1;
+extern alarm_pool_t* HAL_timer_pool_2;
+extern alarm_pool_t* HAL_timer_pool_3;
+
+extern void HAL_timer_0_callback();
+extern void HAL_timer_1_callback();
+extern void HAL_timer_2_callback();
+extern void HAL_timer_3_callback();
+
+extern int64_t HAL_timer_alarm_pool_0_callback(long int, void*);
+extern int64_t HAL_timer_alarm_pool_1_callback(long int, void*);
+extern int64_t HAL_timer_alarm_pool_2_callback(long int, void*);
+extern int64_t HAL_timer_alarm_pool_3_callback(long int, void*);
 
 // ------------------------
 // Public functions
 // ------------------------
 void HAL_timer_init();
 void HAL_timer_start(const uint8_t timer_num, const uint32_t frequency);
+void HAL_timer_stop(const uint8_t timer_num);
 
 FORCE_INLINE static void HAL_timer_set_compare(const uint8_t timer_num, const hal_timer_t compare) {
+  HAL_timer_stop(timer_num);
+
+  switch (timer_num) {
+    case 0:
+      alarm_pool_add_alarm_in_us(HAL_timer_pool_0 ,compare , HAL_timer_alarm_pool_0_callback ,0 ,false );
+      break;
+    
+    case 1:
+      alarm_pool_add_alarm_in_us(HAL_timer_pool_1 ,compare , HAL_timer_alarm_pool_1_callback ,0 ,false );
+      break;
+
+    case 2:
+      alarm_pool_add_alarm_in_us(HAL_timer_pool_2 ,compare , HAL_timer_alarm_pool_2_callback ,0 ,false );
+      break;
+
+    case 3:
+      alarm_pool_add_alarm_in_us(HAL_timer_pool_3 ,compare , HAL_timer_alarm_pool_3_callback ,0 ,false );
+      break;
+  }
 }
 
 FORCE_INLINE static hal_timer_t HAL_timer_get_compare(const uint8_t timer_num) {
@@ -72,21 +108,25 @@ FORCE_INLINE static hal_timer_t HAL_timer_get_compare(const uint8_t timer_num) {
 }
 
 FORCE_INLINE static hal_timer_t HAL_timer_get_count(const uint8_t timer_num) {
-  return 0;
+  return time_us_64();
 }
 
 FORCE_INLINE static void HAL_timer_enable_interrupt(const uint8_t timer_num) {
+  irq_set_enabled(timer_num,true); //lucky coincidence that timer_num and rp2040 irq num matches
 }
 
 FORCE_INLINE static void HAL_timer_disable_interrupt(const uint8_t timer_num) {
+    irq_set_enabled(timer_num,false); //lucky coincidence that timer_num and rp2040 irq num matches
 }
 
 FORCE_INLINE static bool HAL_timer_interrupt_enabled(const uint8_t timer_num) {
-  return false;
+  return irq_is_enabled(timer_num); //lucky coincidence that timer_num and rp2040 irq num matches
 }
 
 FORCE_INLINE static void HAL_timer_isr_prologue(const uint8_t timer_num) {
-
+  irq_clear(timer_num);
 }
 
 #define HAL_timer_isr_epilogue(TIMER_NUM)
+
+//private function
